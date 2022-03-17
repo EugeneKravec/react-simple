466051c983122b5072f07aeefc8484c8b54d9c63
Eugene Kravec <krava255@gmail.com>
2022-03-18 01:23:02 +0200

    Change dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,31 @@
-// import c from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
-    <div className="dialogs">
-      <div className="dialogs-items">
-        <div className="item">Dimych</div>
-        <div className="item">Andrey</div>
-        <div className="item">Sveta</div>
-        <div className="item">Sasha</div>
-        <div className="item">Viktor</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </div>
+
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3">Sveta</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4">Sasha</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5">Viktor</NavLink>
+        </div>
+      </div>
+      <div className="messages">
+        <div className="message">Hi</div>
+        <div className="message">How are your education?</div>
+        <div className="message">Yo-yo</div>
       </div>
     </div>
   );
